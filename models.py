febe28b27342befc6e06a3092c79524f6a34658f
huythanhdang <huythanhdang.dth@gmail.com>
2024-12-29 10:30:24 +0700

    Task 4

--- models.py
+++ models.py
@@ -73,6 +73,19 @@ class NearEarthObject:
         return f"NearEarthObject(designation={self.designation!r}, name={self.name!r}, " \
                f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})"
 
+    def serialize(self):
+        """Return a dict representation of self attributes.
+        
+        Returns:
+            [dict]: Keys associated with self attributes.
+            
+        """
+        return {
+            "designation": self.designation,
+            "name": self.name,
+            "diameter_km": self.diameter,
+            "potentially_hazardous": self.hazardous,
+        }
 
 class CloseApproach:
     """A close approach to Earth by an NEO.
@@ -139,3 +152,16 @@ class CloseApproach:
         """Return `repr(self)`, a computer-readable string representation of this object."""
         return f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, " \
                f"velocity={self.velocity:.2f}, neo={self.neo!r})"
+
+    def serialize(self):
+        """Return a dict representation of self attributes.
+        
+        Returns:
+            [dict]: Keys associated with self attributes.
+            
+        """
+        return {
+            "datetime_utc": datetime_to_str(self.time),
+            "distance_au": self.distance,
+            "velocity_km_s": self.velocity,
+        }
